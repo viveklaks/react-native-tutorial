a6f7cdafbd2fb68767ee77008dd83cc19cdc0897
Vivek H <viveklaks@gmail.com>
2020-06-20 00:42:15 +0530

    missing lesson 6

--- my-project/App.js
+++ my-project/App.js
@@ -1,26 +1,33 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React ,{useState} from 'react';
+import { StyleSheet, Text, View ,ScrollView} from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('shaun');
-  const [age, setAge] = useState('30');
+  const [people,setPeople]= useState([
+    {name:'shaun',key:'1'},
+    {name:'yoshi',key:'2'},
+    {name:'mario',key:'3'},
+    {name:'luigi',key:'4'},
+    {name:'peach',key:'5'},
+    {name:'toad',key:'6'},
+    {name:'bowser',key:'7'}
+  ]);
+  
 
+  
   return (
-    <View style={styles.container}>
-
-      <Text>Enter name:</Text>
-      <TextInput 
-        placeholder='e.g. John Doe' 
-        style={styles.input}
-        onChangeText={(value) => setName(value)} />
-
-      <Text>Enter age:</Text>
-      <TextInput 
-        placeholder='e.g. 99' 
-        style={styles.input}
-        onChangeText={(value) => setAge(value)} />
-
-      <Text style={styles.result}>name: {name}, age: {age}</Text>
+    <View style={styles.container}>  
+    <ScrollView>
+        {
+           people.map(item=>(
+      
+              <View key={item.key}>
+                <Text style={styles.item} >{item.name}</Text>
+              </View>
+      
+           )
+           )
+         } 
+   </ScrollView>
     </View>
   );
 }
@@ -32,14 +39,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200,
-  },
-  result:{
-    fontWeight:'bold',
+  item:{
+   marginTop:24,
+   padding:30,
+   width:400,
+   backgroundColor:'pink' ,
+   fontSize:24,
+   textAlign:'center'
   }
 });
